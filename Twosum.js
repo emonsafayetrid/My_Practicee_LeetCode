29589d4b2753cebaba886e6abd1912b59cefd84b
emonsafayetrid <emonsafayet2148@gmail.com>
2026-06-06 12:29:40 +0600

    Initial commit: Add Two Sum solution

---
+++ Twosum.js
@@ -0,0 +1,12 @@
+let arr=[2,7,11,15];
+let target=18;
+function twoSum(arr,target){
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]===target){
+                return [i,j];
+            }
+        }    }
+    return [];
+}   
+console.log(twoSum(arr,target));
